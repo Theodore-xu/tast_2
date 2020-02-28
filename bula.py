53867c72a3f954973a50abaaab586b19bf4752b7
Theodore-xu <60450914+Theodore-xu@users.noreply.github.com>
2020-02-28 17:17:01 -0500

    my first commit

---
+++ bula.py
@@ -0,0 +1,30 @@
+
+#class presedential_candidate:
+
+	#def __init__(self, name, age, born_date, education, partisan):
+		#self.name 			= name
+		#self.age 			= age
+		#self.born_date 		= born_date
+		#self.education 		= education
+		#self.partisan 	= partisan
+
+
+
+
+
+#def main():
+	
+	#presedential_candidate_1 = presedential_candidate(name = "Andrew Yang", 
+													  #age = 45, born_date = "1/13/1975", 
+													 # education = "Columbia University School of Law", 
+													 # partisan = "Democratic Party")
+
+	#print(presedential_candidate_1.name)
+
+def main():
+	print(100 % 6)
+
+
+
+if __name__ == '__main__':
+	main()
